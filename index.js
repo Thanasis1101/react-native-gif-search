ed2ddff2d63b948c9221585aeb09d32081b2c1e1
thanasis1101 <bollasath@yahoo.gr>
2020-10-18 17:10:10 +0300

    Fixed index.js

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 import GifSearch from './src/GifSearch';
-export { GifSearch };
+
+const poweredByTenorLogoWhite = require('./img/tenor/PB_tenor_logo_white_horizontal.png')
+const poweredByTenorLogoGrey = require('./img/tenor/PB_tenor_logo_grey_horizontal.png')
+const poweredByTenorLogoBlue = require('./img/tenor/PB_tenor_logo_blue_horizontal.png')
+const viaTenorLogoWhite = require('./img/tenor/via_tenor_logo_white.png')
+const viaTenorLogoGrey = require('./img/tenor/via_tenor_logo_grey.png')
+const viaTenorLogoBlue = require('./img/tenor/via_tenor_logo_blue.png')
+const poweredByGiphyLogoGrey = require('./img/giphy/PoweredBy_200px-White_HorizText.png')
+const poweredByGiphyLogoWhite = require('./img/giphy/PoweredBy_200px-Black_HorizText.png')
+
+export {
+    GifSearch,
+    poweredByTenorLogoWhite,
+    poweredByTenorLogoGrey,
+    poweredByTenorLogoBlue,
+    viaTenorLogoWhite,
+    viaTenorLogoGrey,
+    viaTenorLogoBlue,
+    poweredByGiphyLogoGrey,
+    poweredByGiphyLogoWhite
+};
